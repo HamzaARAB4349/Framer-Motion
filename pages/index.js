4b14d8942663aadc8b42e972a5ba0d7c925ca073
HamzaARAB4349 <arabhamza4349@gmail.com>
2024-03-28 21:51:02 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import AnimationControls from "../components/Animationcontrols";
+
 import BasicsOfMotion from "../components/BasicsOfMotion";
-import Gestures from "../components/Gestures";
-import ScrollAnimations from "../components/ScrollAnimation";
-import ViewBasedAnimations from "../components/ViewBaseAnimaions";
+
+
+
 
 export default function Home() {
   return (
